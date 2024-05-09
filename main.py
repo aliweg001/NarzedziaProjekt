c32343bb9f1470eceefefaacc3e9e21786aefc61
admin_git <alicja.wegorowska179@wp.pl>
2024-05-10 00:05:43 +0200

    dodanie komentarzy

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
-import argparse
-import logging
-import requests
-import unittest
+import argparse #importujemy biblioteki obslugujace przekazywanie argumentow z linii komend
+import logging #bibliotegka do logowania bledow
+import requests #importujemy biblioteke do obslugi polecen restowych
 
 logging.basicConfig(level=logging.INFO)
 
-def get_exchange_rate(currency_code):
+def get_exchange_rate(currency_code): #pobieramy stosunek wymiany w zaleznosci od waluty
     try:
         url = f'http://api.nbp.pl/api/exchangerates/rates/a/{currency_code.lower()}/'
         response = requests.get(url)
         response.raise_for_status()
+       # print (response.json())
     except requests.exceptions.RequestException as e:
         logging.error(f'Problem z wymiana walut: {e}')
         raise
     return response.json()['rates'][0]['mid']
 
 
-def exchange_currency(input_value, currency_code):
-    exchange_rate = get_exchange_rate(currency_code)
+def exchange_currency(input_value, currency_code): #funkcja do wymiany waluty podajemy ilosc do wymiany i kod waluty
+    exchange_rate = get_exchange_rate(currency_code) #przelicznik wymiany
     return round(input_value * exchange_rate,2)
 
 
--- tests.py
+++ tests.py
@@ -1,26 +1,28 @@
-import unittest
+import unittest #biblioteka do testow jednostkowych
 from main import get_exchange_rate
 from main import exchange_currency
 
 class ExchangeTest(unittest.TestCase):
-    def setUp(self):
+    def setUp(self): #ustalnenie wartosci do testowania
         self.a = 10
         self.b = "USD"
-    def test_exchange_rate(self):
+    def test_exchange_rate(self): #testujemy funkcje exchange_rate
         print("Test 1")
         #Act
-        result = get_exchange_rate(self.b)
+        result = get_exchange_rate(self.b)  #pobieranie kursu dolara 
         
         #Assert
-        self.assertGreater(result,self.a/self.a)
+        self.assertGreater(result,self.a/self.a)  #sprawdzenie czy kurs dolara jest wiekszy niz jeden
 
-    def test_exchange_currency(self):
+    def test_exchange_currency(self): #testujemy funkcje wymaine walut
         print("Test 2")
         #Act
-        exchange_rate = get_exchange_rate(self.b)
-        result = exchange_currency(self.a,self.b)
+        exchange_rate = get_exchange_rate(self.b)  #sprawdzamy jaki jest kurs wymainy dla dolara
+        result = exchange_currency(self.a,self.b)  #sprawdzamy wartosc wymiany 10 dolarow
         #Assert
-        self.assertEqual(result,round(exchange_rate * self.a,2))
+        self.assertEqual(result,round(exchange_rate * self.a,2)) #sprawdzamy czy wartosci sa takie same przy wykorzystaniu funkcji exchange_currency i bez niej
+
+
 
 
 
